5444c412fbbe628278f8e8e0465902a659e4babf
coderlawyer <87715725+coderlawyer@users.noreply.github.com>
2021-10-30 09:57:53 +0300

    Create odev2.sql

---
+++ odev2.sql
@@ -0,0 +1,5 @@
+SELECT * FROM film where replacement_cost BETWEEN 12.99 AND 16.99;
+
+SELECT first_name,last_name FROM actor where first_name IN ("Penelope","Ed","Nick");
+
+SELECT * FROM film where (rental_rate IN (0.99,2.99,4.99)) AND (replacement_cost IN (12.99, 15.99, 28.99));
