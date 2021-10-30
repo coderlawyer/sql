1286f130fab281c16d7a0a1d09a88dec8bc4d057
coderlawyer <87715725+coderlawyer@users.noreply.github.com>
2021-10-30 10:10:29 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,5 @@
+SELECT country FROM country where country LIKE "A%a";
+
+SELECT country FROM country WHERE country LIKE "_____%n";
+
+SELECT * FROM title where ((length>90) and rental_rate = 2.99) and title LIKE "C%";
