f8530ccb90d00944c26b4a0e61bb8dc7469bb7d2
coderlawyer <87715725+coderlawyer@users.noreply.github.com>
2021-10-31 17:27:17 +0300

    Update odev6.sql

--- odev6.sql
+++ odev6.sql
@@ -4,4 +4,4 @@ SELECT title from film where title like "C%";
 
 SELECT MAX(length) from film where rental_rate = 0.99;
 
-SELECT COUNT(DISTINCT length) from film where length>150;
+SELECT COUNT(DISTINCT replacement_cost) from film where length>150;
